ffc3d022e6c632a1d69311ec3ec73421d404ef70
Saketh-Vadlamudi-TigerAcc <saketh.vadlamudi@tigeranalytics.com>
2025-06-19 15:21:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Hello from Streamlit on Databricks!")
@@ -8,4 +8,14 @@ name = st.text_input("What's your name?", "World")
 st.write(f"Hello, {name}!")
 
 if st.button("Click Me!"):
-    st.write("You clicked the button!") 
+    st.write("You clicked the button!") '''
+import os
+os.environ["PYSPARK_USE_SPARK_CONNECT"] = "0"  # Disable Spark Connect before importing PySpark
+
+from pyspark.sql import SparkSession
+# /editor/files/779215961203655?o=1806740992728685
+# spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.remote("sc://adb-1806740992728685.5.azuredatabricks.net:443").getOrCreate()
+
+df = spark.range(10)
+df.show()
